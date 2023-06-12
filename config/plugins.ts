d1f11e16784a9b3dfe6ef7a314ba2889670d8ddd
Hasibur Rahman <hasib.webdev@gmail.com>
2023-06-12 23:37:36 +0600

    Upldated storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -39,5 +39,25 @@ export default ({env}) => ({
         },
         security: [ { bearerAuth: [] } ]
       }
-    }
+    },
+    upload: {
+      config: {
+          provider: 'strapi-provider-upload-cloudflare-r2',
+          providerOptions: {
+              accessKeyId: env('R2_ACCESS_KEY_ID'),
+              secretAccessKey: env('R2_ACCESS_SECRET'),
+              region: env('R2_REGION'),
+              params: {
+                  Bucket: env('R2_BUCKET'),
+                  accountId: env('R2_ACCOUNT_ID'),
+                  publicUrl: env('R2_PUBLIC_URL'),
+              },
+          },
+          actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+          },
+      },
+  },
   })
